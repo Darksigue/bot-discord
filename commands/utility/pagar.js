818a10ee04798a5b3d4936a150288cf18eedd75d
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-10-29 10:40:20 -0300

    fix: bot messages in payment

--- commands/utility/pagar.js
+++ commands/utility/pagar.js
@@ -1,19 +1,12 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("pagar")
-    .setDescription("Marca uma task como PAGA")
+    .setDescription("Marca uma pessoa com status")
     .addUserOption((option) =>
       option
         .setName("usuario")
         .setDescription("Usuário que completou a task")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("task_id")
-        .setDescription("ID da task (ex: 001)")
         .setRequired(true)
     )
     .addStringOption((option) =>
@@ -23,27 +16,22 @@ module.exports = {
         .setRequired(true)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
-
   async execute(interaction) {
     const usuario = interaction.options.getUser("usuario");
-    const taskId = interaction.options.getString("task_id");
     const status = interaction.options.getString("status");
 
-    console.log(
-      `      💰 Marcando task ${taskId} como ${status} para ${usuario.tag}`
-    );
+    console.log(`💰 Marcando status como ${status} para ${usuario.tag}`);
+
+    await interaction.deferReply({ ephemeral: true });
 
     await interaction.channel.send(
-      `
--# ### Usuário: <@${usuario.id}>
--# ### <:money:1432490151170281603> **task: ${taskId} status: ${status}** <:money:1432490151170281603>`
+      `-# ### Usuário: <@${usuario.id}> **status: ${status}** <:money:1432490151170281603>`
     );
 
-    await interaction.reply({
-      content: `✅ Task ${taskId} marcada como ${status} para ${usuario.username}`,
-      ephemeral: true,
+    await interaction.editReply({
+      content: `Status marcado como ${status} para ${usuario.username}`,
     });
 
-    console.log(`      ✅ Pagamento registrado!`);
+    console.log(`✅ Pagamento registrado!`);
   },
 };
